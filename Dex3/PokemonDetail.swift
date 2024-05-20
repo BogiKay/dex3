832ff87d05c425208d0cd88107e00a168044ad96
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-05-20 19:06:22 +0200

    stats model

--- Dex3/PokemonDetail.swift
+++ Dex3/PokemonDetail.swift
@@ -10,16 +10,17 @@ import CoreData
 
 struct PokemonDetail: View {
     @EnvironmentObject var pokemon: Pokemon
+    @State var showShiny = false
 
     var body: some View {
         ScrollView {
             ZStack {
-                Image("normalgrasselectricpoisonfairy")
+                Image(pokemon.background)
                     .resizable()
                     .scaledToFit()
                     .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 6)
                 
-                AsyncImage(url: pokemon.sprite) { image in
+                AsyncImage(url: showShiny ? pokemon.shiny : pokemon.sprite) { image in
                     image
                         .resizable()
                         .scaledToFit()
@@ -43,6 +44,21 @@ struct PokemonDetail: View {
                 Spacer()
             }
             .padding()
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    showShiny.toggle()
+                } label: {
+                    if showShiny {
+                        Image(systemName: "wand.and.stars")
+                            .foregroundColor(.yellow)
+                    } else {
+                        Image(systemName: "wand.and.stars.inverse")
+                    }
+                }
+            }
         }
     }
 }
