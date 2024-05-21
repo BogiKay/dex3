0d3415e02eb27157a8f1f9b2374370eb16d531b4
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-05-21 20:24:05 +0200

    init widgets

---
+++ Dex3/FetchedImage.swift
@@ -0,0 +1,27 @@
+//
+//  FetchedImage.swift
+//  Dex3
+//
+//  Created by Bogusz Kaszowski on 21/05/2024.
+//
+
+import SwiftUI
+
+struct FetchedImage: View {
+    let url: URL?
+    
+    var body: some View {
+        if let url, let imageData = try? Data(contentsOf: url), let uiImage = UIImage(data: imageData) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 6)
+        } else {
+            Image("bulbasaur")
+        }
+    }
+}
+
+#Preview {
+    FetchedImage(url: SamplePokemon.samplePokemon.sprite)
+}
---
+++ Dex3Widget/Dex3WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Dex3WidgetBundle.swift
+//  Dex3Widget
+//
+//  Created by Bogusz Kaszowski on 21/05/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Dex3WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Dex3Widget()
+        Dex3WidgetLiveActivity()
+    }
+}
